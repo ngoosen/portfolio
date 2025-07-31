b91749a0d756b45dfb604a6b29f81a71ddfd2b7d
Noémie Goosen <noemie.goosen@gmail.com>
2025-07-31 16:25:02 +0200

    feat: add screen component

---
+++ app/components/Screen.tsx
@@ -0,0 +1,23 @@
+import { ReactNode } from "react";
+
+import styles from "../style/Screen.module.scss";
+
+interface SCREEN_PROPS {
+  children: ReactNode
+  color: string;
+}
+
+export default function Screen(props: SCREEN_PROPS) {
+  const {
+    children,
+    color,
+  } = props;
+
+  return (
+    <article className={styles.main}
+      style={{ backgroundColor: color, }}
+    >
+      {children}
+    </article>
+  );
+}
