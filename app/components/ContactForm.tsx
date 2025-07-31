51700df7139706b764aa617004f289cd7fc1b17c
Noémie Goosen <noemie.goosen@gmail.com>
2025-07-31 17:15:55 +0200

    feat: add contact form component base

---
+++ app/components/ContactForm.tsx
@@ -0,0 +1,11 @@
+import styles from "../style/ContactForm.module.scss";
+
+export default function ContactForm() {
+  return (
+    <form className={styles.main}>
+      <h1>Test</h1>
+      <h2>Test test test</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi sint id debitis nihil. Cupiditate nesciunt ullam eos dolores quis nihil ratione et, itaque porro possimus quae dolorem non pariatur. Id!</p>
+    </form>
+  );
+}
--- app/components/ContactScreen.tsx
+++ app/components/ContactScreen.tsx
@@ -1,10 +1,13 @@
 import styles from "../style/ContactScreen.module.scss";
+import ContactForm from "./ContactForm";
 
 export default function ContactScreen() {
   return (
     <article className={styles.main}>
       <section>
       </section>
+
+      <ContactForm />
     </article>
   );
 }
