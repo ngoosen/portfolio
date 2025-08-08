8fd7e4bdc4e8df41c4c72cb44ed4f6bd9bae1136
Noémie Goosen <noemie.goosen@gmail.com>
2025-08-08 16:16:14 +0200

    feat: add montreal font and add it to footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,18 @@
+import localFont from 'next/font/local';
 import styles from "../style/Footer.module.scss";
+
+const montrealBold = localFont({ src: "../_assets/fonts/Montreal-Bold.ttf" });
 
 export default function Footer() {
   return (
-    <footer className={styles.footer}></footer>
+    <footer className={styles.footer}>
+      <section></section>
+      <section className={styles.title}>
+        <h1 className={montrealBold.className}>
+          <span>Noémie</span>
+          <span>Goosen</span>
+        </h1>
+      </section>
+    </footer>
   )
 }
