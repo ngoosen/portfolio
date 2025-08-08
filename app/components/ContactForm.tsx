c799aa010aa6590db3634751850d1679828dd2fb
Noémie Goosen <noemie.goosen@gmail.com>
2025-08-08 17:10:49 +0200

    style: add montreal font to form labels

--- app/components/ContactForm.tsx
+++ app/components/ContactForm.tsx
@@ -1,8 +1,13 @@
 "use client"
 
+import localFont from "next/font/local";
 import { ChangeEvent, SyntheticEvent, useState } from "react";
+
 import styles from "../style/ContactForm.module.scss";
+
 import FunButton from "./ui/FunButton";
+
+const montrealBold = localFont({ src: "../_assets/fonts/Montreal-Bold.ttf" });
 
 export default function ContactForm() {
   const [firstname, setFirstname] = useState<string>("");
@@ -57,24 +62,24 @@ export default function ContactForm() {
         <form className={styles.form} onSubmit={formSubmitHandler}>
           <section>
             <article>
-              <input type="text" id="firstname" required value={firstname} onChange={valueChangeHandler} />
-              <label htmlFor="firstname">Firstname :</label>
+              <input type="text" id="firstname" required value={firstname} onChange={valueChangeHandler} maxLength={35} />
+              <label htmlFor="firstname" className={montrealBold.className}>Firstname :</label>
             </article>
             <article>
-              <input type="text" id="lastname" required value={lastname} onChange={valueChangeHandler} />
-              <label htmlFor="lastname">Lastname :</label>
+              <input type="text" id="lastname" required value={lastname} onChange={valueChangeHandler} maxLength={35} />
+              <label htmlFor="lastname" className={montrealBold.className}>Lastname :</label>
             </article>
           </section>
           <section>
             <article>
-              <input type="email" name="email" id="email" required value={email} onChange={valueChangeHandler} />
-              <label htmlFor="email">Email :</label>
+              <input type="email" name="email" id="email" required value={email} onChange={valueChangeHandler} maxLength={254} />
+              <label htmlFor="email" className={montrealBold.className}>Email :</label>
             </article>
           </section>
           <section>
             <article>
               <textarea name="message" id="message" rows={8} required value={message} onChange={messageValueChangeHandler}></textarea>
-              <label htmlFor="message">Message :</label>
+              <label htmlFor="message" className={montrealBold.className}>Message :</label>
             </article>
           </section>
 
