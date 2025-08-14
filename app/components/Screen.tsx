da9cd64afacc25c4d47e151dfae8c740cf334aa6
Noémie Goosen <noemie.goosen@gmail.com>
2025-08-14 13:50:18 +0200

    feat: create image folder in public

--- app/components/Screen.tsx
+++ app/components/Screen.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 import styles from "../style/Screen.module.scss";
 
 interface SCREEN_PROPS {
-  children: ReactNode
+  children: ReactNode | ReactNode[]
   color: string;
 }
 
